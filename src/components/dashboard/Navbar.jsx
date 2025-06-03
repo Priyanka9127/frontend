1538edea08e5356a1be0ab11549422f3fbb11b74
Priyanka Kumari <priyankasimmi1309@gmail.com>
2025-06-03 15:07:27 +0530

    Update Navbar.jsx

--- src/components/dashboard/Navbar.jsx
+++ src/components/dashboard/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     const {user, logout} = useAuth()
   return (
     // Changed bg-teal-600 to bg-red-700
-    <div className='flex items-center text-white justify-between h-12 bg-red-600 px-5'> 
+    <div className='flex items-center text-white justify-between h-12 px-5'> 
         <p>Welcome {user.name}</p>
         {/* Changed bg-teal-700 to bg-red-800 and hover:bg-teal-800 to hover:bg-red-900 */}
         <button className='px-4 py-1 bg-red-800 hover:bg-red-900 rounded mx-2' onClick={logout}>Logout</button>
